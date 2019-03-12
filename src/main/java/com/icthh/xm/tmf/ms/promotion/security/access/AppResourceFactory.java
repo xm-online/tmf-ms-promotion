faa5806ca77ceb60894930d305cf61e49ac63b3b
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2019-03-12 18:59:44 +0200

    clean code

--- src/main/java/com/icthh/xm/tmf/ms/promotion/security/access/AppResourceFactory.java
+++ src/main/java/com/icthh/xm/tmf/ms/promotion/security/access/AppResourceFactory.java
@@ -1,4 +1,4 @@
-package com.icthh.xm.tmf.ms.promotion.config;
+package com.icthh.xm.tmf.ms.promotion.security.access;
 
 import com.icthh.xm.commons.permission.access.ResourceFactory;
 import org.springframework.stereotype.Component;
